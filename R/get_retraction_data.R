c925f2ab600cdd2110ffe8adbae87ae8276f51d4
Mark Engelbert <mengelbert@3ieimpact.org>
2025-01-23 09:15:28 +0000

    Add a file to the "data" folder specifying when Retraction Watch data were last updated.

--- R/get_retraction_data.R
+++ R/get_retraction_data.R
@@ -1,10 +1,13 @@
 # Sources RetractionWatch data from Crossref GitLab as recommended (as of 2024-12-19) at: https://www.crossref.org/documentation/retrieve-metadata/retraction-watch/  
 
-#object for url
+# Object for url
 url <- "https://gitlab.com/crossref/retraction-watch-data/-/raw/main/retraction_watch.csv?ref_type=heads"
 
-#read csv from url
+# Read csv from url
 retwatch_db<-read.csv(url)
 
-#update the csv in the 'data' directory
+# Update the csv in the 'data' directory
 write.csv(retwatch_db,  file = 'data/retraction_watch.csv')
+
+# Add a file describing when the data were last updated
+cat("Retraction Watch data last retrieved on ", Sys.time(), file = "latest_update.txt")
