8c7dd16c65e447780a6f9137434141f37bd7dbde
Matthew Grainger <DrMattG@users.noreply.github.com>
2025-05-07 17:19:06 +0200

    Add runShiny option

---
+++ R/runShiny.R
@@ -0,0 +1,18 @@
+#' A wrapper function to run the Shiny App 
+#' @return A shiny app
+#' 
+#' @import shiny
+#' @import retractionfindr
+#' @import synthesisr
+#' @import tidyverse
+#' @import shinyBS
+#' 
+#' @export
+
+runShiny <- function(){
+  
+  # find and launch the app
+  appDir <- system.file("RetractionFindR", package = "RetractionFindR")
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
